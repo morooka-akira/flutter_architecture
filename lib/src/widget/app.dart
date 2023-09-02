e80bde9a1e3f2a382d1b2f6b048b1d82a53b1910
Heiya <morooka.akira.inon29@gmail.com>
2023-09-02 10:30:29 +0900

    main.dart/app.dart/pagesに分離

---
+++ lib/src/widget/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'themes/app_theme_data.dart';
+
+RouteFactory createRouter() {
+  return (RouteSettings setting) {
+    return MaterialPageRoute(
+      builder: (context) => const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  };
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter architecture sample',
+      theme: appThemeData,
+      onGenerateRoute: createRouter(),
+    );
+  }
+}
---
+++ lib/src/widget/themes/app_theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData get appThemeData {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+  );
+}
